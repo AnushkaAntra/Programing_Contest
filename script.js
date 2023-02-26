f632689124347347911d66e3258fd1c72d42beba
Anushka Antra <116517571+AnushkaAntra@users.noreply.github.com>
2023-02-26 17:28:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,151 @@
+/*let x = document.getElementsByTagName("nav")[0].firstElementChild
+x.style.color = "yellow"
+let y = document.getElementsByClassName("box")[0].firstElementChild
+y.style.color = "green"
+let z = document.getElementsByClassName("box")[0].lastElementChild
+z.style.color = "green"
+document.getElementById("h2").style.color = "purple"
+Array.from(document.getElementsByTagName("li")).forEach((element) =>{
+    element.style.background = "black";
+})*/
+//document.querySelectorAll("li").style.background = "black"; ---> not possible
+
+/*function random(min, max){
+    return Math.floor(Math.random()*(max-min+1)+min)
+}
+let arr = [
+    `How many programmers does it take to change a light bulb?
+    None – It’s a hardware problem`,
+
+    
+    `Why do programmers always mix up Halloween and Christmas?
+    Because Oct 31 equals Dec 25.`,
+
+    
+    `There are only 10 kinds of people in this world: those who know binary and those who don’t.`,
+
+    
+    `A programmer walks to the butcher shop and buys a kilo of meat.  An hour later he comes back upset that the butcher shortchanged him by 24 grams.`,
+
+    
+    `“Knock, knock.”
+    “Who’s there?”
+    very long pause….
+    “Java.”`,
+
+    
+    `Programming is 10% science, 20% ingenuity, and 70% getting the ingenuity to work with the science.`,
+
+    
+    `Programming is like sex:
+    One mistake and you have to support it for the rest of your life.`,
+
+    
+    `A man is smoking a cigarette and blowing smoke rings into the air.  His girlfriend becomes irritated with the smoke and says, “Can’t you see the warning on the cigarette pack?  Smoking is hazardous to your health!”
+    
+    To which the man replies, “I am a programmer.  We don’t worry about warnings; we only worry about errors.”`,
+
+    
+    `There are three kinds of lies: Lies, damned lies, and benchmarks.`,
+
+    
+    `A programmer is walking along a beach and finds a lamp.  He rubs the lamp, and a genie appears.  “I am the most powerful genie in the world.  I can grant you any wish, but only one wish.”
+    
+    The programmer pulls out a map, points to it and says, “I’d want peace in the Middle East.”
+    
+    The genie responds, “Gee, I don’t know.  Those people have been fighting for millennia.  I can do just about anything, but this is likely beyond my limits.”
+    
+    The programmer then says, “Well, I am a programmer, and my programs have lots of users.  Please make all my users satisfied with my software and let them ask for sensible changes.”
+    
+    At which point the genie responds, “Um, let me see that map again.”`,
+
+    
+    `All programmers are playwrights, and all computers are lousy actors.`,
+
+    
+    `Have you heard about the new Cray super computer?  It’s so fast, it executes an infinite loop in 6 seconds.`,
+
+    
+    `The generation of random numbers is too important to be left to chance.`,]
+let a = random(0, arr.length)*/
+/*document.getElementsByClassName("jokes")[0].innerHTML = `The joke is : <q>${arr[a]}</q>`
+
+let p = document.getElementsByClassName("container")[0]
+let div1 = document.createElement("div")
+div1.innerHTML = "<h1>Good Morning, I'm Anushka Antra</h1>"
+p.appendChild(div1)
+
+function my_function1(){
+    alert("hello world!")
+}
+let btn1 = document.getElementById("btn1")
+btn1.addEventListener("click", function(){
+    alert(`the joke will be: ${arr[a]}`)
+})
+function my_function2(){
+    alert("congo")
+}
+function my_function3(){
+    alert("Bye bye")
+}
+
+let google = document.getElementById("google")
+google.addEventListener("click",function(){
+    window.open("https://www.google.co.in")
+})
+
+let m = setIntervel(function(){
+    alert("good morning")
+}, 3000)
+
+let elem = document.getElementsByTagName("div")[0]
+setInterval(async function(){
+    elem.classList.toggle("yellow")
+},300)*/
+
+/*let date = new Date()
+let hours = document.getElementById("hours")
+let minutes = document.getElementById("minutes")
+let seconds = document.getElementById("seconds")
+let date1 = document.getElementById("date")
+let h = date.getHours()
+hours.innerHTML = `<center>${h}</center>`
+let m = date.getMinutes()
+minutes.innerHTML = `<center>${m}</center>`
+let s = date.getSeconds()
+seconds.innerHTML = `<center>${s}</center>`
+let d = date.getDate()
+date1.innerHTML = `<center>${d}</center>`*/
+
+let array = [
+    "https://images.techopedia.com/images/uploads/istock-1173805290.jpeg?w=800&h=0&mode=max&quality=70&scale=both", "https://www.thewomenachiever.com/wp-content/uploads/2021/12/Coding-Resources-for-Women.jpg", "https://t3.ftcdn.net/jpg/03/92/74/38/360_F_392743806_ZcUdSiI7ZMinGrndZ4c3X8upA7wXx9gN.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe6-SG2vy4jvsRN7oZOm0o1e47paN_MTCuzMVnCXBFiqqqt0eSYV-hNmVWG_IGGeXXZ4A&usqp=CAU", "https://www.shutterstock.com/image-photo/close-woman-hands-coding-html-260nw-1094703257.jpg", "https://www.shutterstock.com/image-photo/programmer-girl-works-computer-260nw-1610721214.jpg", "https://www.shutterstock.com/image-photo/young-woman-working-computer-table-260nw-753780223.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR27nSHaWT3JlZ7YX3rV2UhZ3IjnlsNsNAbiC2xYaK1Lqch5v2XvVHEav7lk3Uy51Omk_o&usqp=CAU", "https://media.gettyimages.com/id/1212006391/photo/computer-programmer-working-on-new-software-program.jpg?s=612x612&w=gi&k=20&c=wxLXPgamuSXPQl00qCh7jsWz9kmG5sxbC9hmQGVovAo=", "https://media.gettyimages.com/id/1265176772/photo/computer-programmer-working-on-laptop.jpg?s=612x612&w=gi&k=20&c=lADzOsY4JxQrL2XWjMHKfyh6YUCi53dL72TzpviKRE0=", "https://images.theconversation.com/files/479421/original/file-20220816-10908-uvh62x.jpg?ixlib=rb-1.1.0&rect=4%2C5%2C994%2C497&q=45&auto=format&w=668&h=324&fit=crop", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKOqnPo9lG2k6nZoPA4djBkvDbJkDiwJTSr4TIlypo2vM4bW_FHj3L4hn28TqZnkHlAb0&usqp=CAU", "https://media.istockphoto.com/id/1221204650/photo/business-can-not-wait.jpg?s=612x612&w=0&k=20&c=1ZJn_BWN-OqIlnjohELZnsUlaXsw9LsKHkUDS8mHfhY=", "https://cdn.mos.cms.futurecdn.net/EdvFtgReiC7EBzjrsdAXve-320-80.jpg", "https://media.istockphoto.com/id/1147195672/photo/focused-developer-coding-on-computer-monitors-working-late-in-office.jpg?s=612x612&w=0&k=20&c=R6oPQ_vkXAxCzpi4UFsN28pdU0C0LJL8JLAX-HwO90Q=", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsPaWYkRfDn1p_sZLUleuPwWeU03-CpivYeQeyEpBGCX8sJane4ZWYwfcSlqnqupR5LbU&usqp=CAU"
+]
+function random(max, min){
+    return Math.floor(Math.random()*(max-min+1)+min)
+}
+let a
+let url = "https://kontests.net/api/v1/all"
+let p = fetch(url)
+p.then((response)=>{
+    return response.json()
+}).then((response)=>{
+    console.log(response)
+    let ihtml = ""
+    for(i in response){
+        console.log(response[i])
+        ihtml += `
+            <div class="card" style="width: 22rem; margin:10px;">
+                   <img src="${array[a = random(0,15)]}" class="card-img-top" alt="...">
+                   <div class="card-body">
+                      <h5 class="card-title">Contest: ${response[i].name}</h5>
+                      <p class="card-text">About contest: </p>
+                      <p class="card-text">Start-time: ${response[i].start_time}</p>
+                      <p class="card-text">End-time: ${response[i].end_time}</p>
+                      <a href="${response[i].url}" class="btn btn-primary">Visit Here</a>
+                  </div>
+            </div>  `
+    }
+    cardContainer.innerHTML = ihtml
+})
+
+
